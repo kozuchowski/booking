d74add99ead8749ecaf4f44db66b94eeec4ad3a1
Marek Kożuchowski <marekkozuchowski4@gmail.com>
2023-01-31 13:45:41 +0100

    Added Tenant model

--- src/main/java/com/booking/booking/model/Tenant.java
+++ src/main/java/com/booking/booking/model/Tenant.java
@@ -1,2 +1,20 @@
-package com.booking.booking.model;public class Tenant {
+package com.booking.booking.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Table(name="TENANTS")
+public class Tenant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
 }
